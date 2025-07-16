a97df1b5ff07dc54e1a119588dee3beccb284eb9
Kerwin <141232455+milkfish033@users.noreply.github.com>
2025-07-16 14:51:46 +0800

    Update stack.py

--- pyalgolab/stack.py
+++ pyalgolab/stack.py
@@ -11,3 +11,14 @@ class Solution:
             stack.append(i)
         return res
 
+#https://leetcode.cn/problems/next-greater-element-i/solutions/1065517/xia-yi-ge-geng-da-yuan-su-i-by-leetcode-bfcoj/
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = {}
+        stack = []
+        for num in reversed(nums2):
+            while stack and num >= stack[-1]:
+                stack.pop()
+            res[num] = stack[-1] if stack else -1
+            stack.append(num)
+        return [res[num] for num in nums1]
