79ecdd40011212b46c4d7f220fc2fa8c09449305
milkfish033 <mingyu@bu.edu>
2025-09-24 20:52:56 -0400

    恰好为k的情况

--- pyalgolab/sliding_window.py
+++ pyalgolab/sliding_window.py
@@ -515,3 +515,23 @@ class Solution:
                 left += 1
             ans += left
         return ans
+
+#https://leetcode.cn/problems/count-number-of-nice-subarrays/
+#O(n)
+#O(1)
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        def s(t): #>=s
+            left = ans = 0
+            cur = 0
+            for right, x in enumerate(nums):
+                if x % 2 == 1:
+                    cur += 1
+                while cur >= t:
+                    if nums[left] % 2 == 1:
+                        cur -= 1
+                    left += 1
+                ans += left
+            return ans
+        
+        return (s(k))-(s(k+1))
