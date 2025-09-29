34ba663decb77777abe8af58a5344033da92f770
milkfish033 <mingyu@bu.edu>
2025-09-29 14:14:06 -0400

    example2

--- pyalgolab/binary_search.py
+++ pyalgolab/binary_search.py
@@ -15,6 +15,8 @@ def binary_search(arr, target): #return the ***first*** position of first number
 # <  : (>=x) - 1
 # <= : (>x) - 1
 
+
+#--------------------------------------------------------------------------------------------------------------
 #https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/666881976/
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
@@ -34,3 +36,22 @@ class Solution:
         end = b(target+1) - 1 # > target左边的第一个数
         return [start, end]
 
+
+#https://leetcode.cn/problems/search-insert-position/submissions/667086210/
+#O(logn)
+#O(1)
+#对于这道题，注意对题目要求的转化，和插入目标数值之后每个元素index的变化
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        def b(t):
+            left, right = -1, len(nums)
+            while left + 1 < right:
+                mid = (right + left)//2
+                if nums[mid] < t:
+                    left = mid
+                else:
+                    right = mid
+            return right 
+        
+        ans = b(target)
+        return ans 
