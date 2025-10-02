72f56a4813112ea8fec7ac5075d643f2a3c40b90
milkfish033 <mingyu@bu.edu>
2025-10-02 17:30:03 -0400

    add comments

--- bisect/min.py
+++ bisect/min.py
@@ -2,16 +2,19 @@
 # if check: right = mid
 # else: left = mid 
 # note: how to initialize left, right 
+#right: legal 
+#left: illegal
 
 
 #example 
 #https://leetcode.cn/problems/find-the-smallest-divisor-given-a-threshold/description/
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
-        left, right = 0, max(nums)
+        left, right = 0, max(nums)  
         while left + 1 < right:
-            mid = (left + right) // 2
-            if sum ((x-1) // mid for x in nums ) <= threshold - len(nums):
+            mid = (left + right) // 2 
+            #note here  //mid should be included in sum() since we are using //
+            if sum ((x-1) // mid for x in nums ) <= threshold - len(nums): #上取整公式推导 
                 right = mid 
             else:
                 left = mid 
