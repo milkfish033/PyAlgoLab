19ea374bb615e2de4ebc48839a591a2424905844
milkfish033 <mingyu@bu.edu>
2025-10-03 17:41:07 -0400

    example for max problem

---
+++ bisect/max.py
@@ -0,0 +1,22 @@
+#求最小
+# if check: left = mid
+# else: right = mid 
+# note: how to initialize left, right 
+# right: illegal 
+# left: legal
+# return left 哪个合法return哪个
+
+#https://leetcode.cn/problems/h-index-ii/description/
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        left, right = 0, n+1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if citations[n-mid] >= mid: 
+                left = mid
+            else:
+                right = mid
+        return left
+
+
