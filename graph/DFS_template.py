efcb52197243aa336122ab19cbca462aec1c4d0e
milkfish033 <mingyu@bu.edu>
2025-10-10 16:23:10 -0400

    rename

---
+++ graph/DFS_template.py
@@ -0,0 +1,24 @@
+def solve(n: int, edges: List[List[int]]) -> List[int]:
+    # 节点编号从 0 到 n-1
+    g = [[] for _ in range(n)]
+    for x, y in edges:
+        g[x].append(y)
+        g[y].append(x)  # 无向图
+
+    vis = [False] * n
+
+    def dfs(x: int) -> int:
+        vis[x] = True  # 避免重复访问节点
+        size = 1
+        for y in g[x]:
+            if not vis[y]:
+                size += dfs(y)
+        return size
+
+    # 计算每个连通块的大小
+    ans = []
+    for i, b in enumerate(vis):
+        if not b:  # i 没有访问过
+            size = dfs(i)
+            ans.append(size)
+    return ans
